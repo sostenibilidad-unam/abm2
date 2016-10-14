72ca8570ef4b80a3aa7909308b99b650ecf1ea30
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-10-14 14:59:31 -0500

    main experiment is always "experiment"

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 java -Xmx8024m -cp ~/bin/netlogo/app/NetLogo.jar org.nlogo.headless.Main \
      --model ABM_V2.0.nlogo \
-     --experiment prueba \
+     --experiment experiment \
      --setup-file $1 \
      --table $2 \
      --threads $3
