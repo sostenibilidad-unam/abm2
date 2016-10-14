d80596eabe5ad22df87cc0a25eebe30bda76b128
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-10-14 17:40:07 -0500

    create links to all dependencies, into workdir

--- vhu.py
+++ vhu.py
@@ -1,7 +1,10 @@
 import argparse
 import random
+import os
 
-parser = argparse.ArgumentParser(description='Create simulation setup file')
+parser = argparse.ArgumentParser(description='Create meta-simulation package at workdir')
+parser.add_argument('--netlogo', required=True, help='absolute path to netlogo directory')
+parser.add_argument('--workdir', required=True, help='absolute path to working directory where meta-simulation will be setup.')
 parser.add_argument('--argumentspace', type=argparse.FileType('r'), required=True,
                     help="argument space CSV file")
 parser.add_argument('--landscape', default="many-hills",
@@ -13,14 +16,35 @@ parser.add_argument('--threads', type=int, default=12)
 args = parser.parse_args()
 
 
+
+# create working directory
+if not os.path.isdir(args.workdir):
+    os.makedirs(args.workdir)
+
+# create symlinks to netlogo and extensions
+netlogo_jar = os.path.join( args.netlogo, "app/NetLogo.jar")
+netlogo_csv = os.path.join( args.netlogo, "app/extensions/csv")
+netlogo_matrix = os.path.join( args.netlogo, "app/extensions/matrix")
+
+assert os.path.exists(netlogo_jar)
+assert os.path.exists(netlogo_csv)
+assert os.path.exists(netlogo_matrix)
+
+os.symlink(netlogo_jar, os.path.join(args.workdir, "NetLogo.jar"))
+os.symlink(netlogo_csv, os.path.join(args.workdir, "csv"))
+os.symlink(netlogo_matrix, os.path.join(args.workdir, "matrix"))
+
+# create symlinks to model, argumentspace and run script
+this_dir = os.path.dirname(os.path.realpath(__file__))
+os.symlink(os.path.join(this_dir, "ABM_V2.0.nlogo"), os.path.join(args.workdir, "ABM_V2.0.nlogo"))
+os.symlink(os.path.join(this_dir, args.argumentspace.name), os.path.join(args.workdir, args.argumentspace.name))
+os.symlink(os.path.join(this_dir, "run.sh"), os.path.join(args.workdir, "run.sh"))
+
+# read setup and submit templates
+setup_template = open('setup_template.xml').read()
+condor_template= open('submit_template.condor').read()
+
 lines = args.argumentspace.readlines()
-
-# TODO: use jinja or something
-setup_template = open('../setup_template.xml').read()
-# TODO: create condor template
-condor_template= open('../submit_template.condor').read()
-
-# TODO: maybe simlink extensions?
 
 # create setup XML files and condor files
 for simulation_number in range(len(lines)-1):
@@ -31,8 +55,8 @@ for simulation_number in range(len(lines)-1):
                                             maintenance,
                                             args.landscape,
                                             simulation_number)
-            with open('setup_%s.xml' % run_id, 'w') as setupfile, \
-                 open('submit_%s.condor' % run_id, 'w') as condorfile:
+            with open('%s/setup_%s.xml' % (args.workdir, run_id), 'w') as setupfile, \
+                 open('%s/submit_%s.condor' % (args.workdir, run_id), 'w') as condorfile:
                 setupfile.write(setup_template.format(simulation_number=simulation_number,
                                                       new_infra_investment=new_infra_investment,
                                                       maintenance=maintenance,
