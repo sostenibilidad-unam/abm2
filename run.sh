1dfe37e45290dbca7e8299053a2cc14e2703bf19
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2016-10-14 17:39:06 -0500

    NetLogo.jar will be symlinked to workdir

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-java -Xmx8024m -cp ~/bin/netlogo/app/NetLogo.jar org.nlogo.headless.Main \
+java -Xmx8024m -cp NetLogo.jar org.nlogo.headless.Main \
      --model ABM_V2.0.nlogo \
      --experiment experiment \
      --setup-file $1 \
