9f2d4731c07b0dcbe78e7c31edfd1821da4d1c27
abaezacastro <andres.baeza@gmail.com>
2016-11-04 10:30:35 -0700

    new parameter space

--- Generate_parameterSpaceforWeights.R
+++ Generate_parameterSpaceforWeights.R
@@ -1,5 +1,9 @@
 require(tgp)
-s1 <- round(lhs(500, rbind(c(0,0.9), c(0,1),c(0,1),c(0,1),c(0,1),c(0,1),c(0,1),c(0,1))),digits = 1)
+s1 <- round(lhs(20000, rbind(c(0,1), c(0,1),c(0,1),c(0,1),c(0,1),c(0,1),c(0,1),c(0,1))),digits = 2)
+
+
+colnames(s1)<-c("w_11_demanda_F",	"w_12_presion_F",	"w_21_necesidad_F",	"w_22_presion_F",	"w_31_demanda_S",	"w_32_presion_S",	"w_41_necesidad_S",	"w_42_presion_S")
+
 s2<-  s1[which(s1[,1]+s1[,2] < 1),]
 s3<-s2[which(s2[,3]+s2[,4]<1),]
 s4<-s3[which(s3[,5]+s3[,6]< 1),]
