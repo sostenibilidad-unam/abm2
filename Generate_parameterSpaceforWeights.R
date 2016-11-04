7cf591dd614634ca8c63bce552a8ebc5cda3a1dd
abaezacastro <andres.baeza@gmail.com>
2016-11-04 10:40:08 -0700

    new sampling space

--- Generate_parameterSpaceforWeights.R
+++ Generate_parameterSpaceforWeights.R
@@ -8,4 +8,4 @@ s2<-  s1[which(s1[,1]+s1[,2] < 1),]
 s3<-s2[which(s2[,3]+s2[,4]<1),]
 s4<-s3[which(s3[,5]+s3[,6]< 1),]
 s5<-s4[which(s4[,7]+s4[,8]< 1),]
-write.csv(x = s5,file = "~/MEGADAPT/ABM_V2/sampling_scenarios_Weights.csv")
+write.csv(x = s5,file = "sampling_scenarios_WeightsLarge.csv")
