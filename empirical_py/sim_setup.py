fff164daf4c1a29c822319f746e6430013629041
Fidel Serrano <serranoycandela@gmail.com>
2017-03-03 18:11:31 -0600

    all lowercases

--- empirical_py/sim_setup.py
+++ empirical_py/sim_setup.py
@@ -12,7 +12,7 @@ import model
 ################################
 parser = argparse.ArgumentParser(description='Setup party simulation by creating and populating groups and guests.')
 parser.add_argument('--db_url', default='sqlite:///:memory:', help='DB URL, default: sqlite:///:memory:')
-parser.add_argument('--inittable', default='ageb8', help='name of init table')
+parser.add_argument('--inittable', default='agebs_all_attributes', help='name of init table')
 parser.add_argument('--sacmex_matrix', type=argparse.FileType('r'), required=True, help='path to SACMEX matrix')
 parser.add_argument('--xochimilco_matrix', type=argparse.FileType('r'), required=True, help='path to Xochimilco matrix')
 parser.add_argument('--iztapalapa_matrix', type=argparse.FileType('r'), required=True, help='path to Ixtapalapa matrix')
@@ -41,28 +41,28 @@ conn = engine.connect()
 s = text("select * from %s" % args.inittable)
 for row in conn.execute(s).fetchall():
     ageb = dict(row)
-    residentMatrixPath = ""
-    if  (ageb['KMEANS5'] == 1) or (ageb['KMEANS5'] == 3):
-        residentMatrixPath = xochimilco_matrix.name
-    if (ageb['KMEANS5'] == 2) or (ageb['KMEANS5'] == 0):
-        residentMatrixPath = iztapalapa_matrix.name
-    if (ageb['KMEANS5'] == 4):
-        residentMatrixPath = contreras_matrix.name
 
-
+    if  (ageb['kmeans5'] == 1) or (ageb['kmeans5'] == 3):
+        residentMatrixPath = args.xochimilco_matrix.name
+    elif (ageb['kmeans5'] == 2) or (ageb['kmeans5'] == 0):
+        residentMatrixPath = args.iztapalapa_matrix.name
+    elif (ageb['kmeans5'] == 4):
+        residentMatrixPath = args.contreras_matrix.name
+    else:
+        continue
     
     a = model.AGEB(args.sacmex_matrix.name, residentMatrixPath)
     a.ID = ageb['ageb_id']
-    a.poblacion = ageb['POBTOT'] if ageb['POBTOT'] > 0 else 1
-    a.houses_with_abastecimiento = ageb['VPH_AGUADV'] / (1 + ageb['VPH_AGUADV'] + ageb['VPH_AGUAFV'])
-    a.houses_with_dranage = ageb['VPH_DRENAJ']  / (1 + ageb['VPH_DRENAJ']  + ageb['VPH_NODREN'] ) 
-    ## a.hundimientos = ageb['HUND']
-    ##a.group_kmean = ageb['KMEANS5']
-    a.disease_burden = (ageb["N04_TODAS"] + ageb["X05_TOTAL"] + ageb["X06_TODAS"] + ageb["X07_TODAS"]+ ageb["X08_TODAS"]+ ageb["X09_TODAS"]+ ageb["N10_TODAS"]+ ageb["N11_TODAS"]+ ageb["N12_TODAS"] + ageb["N13_TODAS"] + ageb["N14_TODAS"]) / 11 
-    a.income_index = 0 if ageb['I05_INGRES'] == 'nobody' else ageb['I05_INGRES'] #nobody????
-    ##a.flooding = 0 if ageb['Mean_encha'] == 'nobody' else ageb['Mean_encha']
-    ##a.antiguedad_infra = ageb['edad_infra']
-    ##a.zona_aquifera = ageb['zonas']
+    a.poblacion = ageb['pobtot'] if ageb['pobtot'] > 0 else 1
+    a.houses_with_abastecimiento = ageb['vph_aguadv'] / (1 + ageb['vph_aguadv'] + ageb['vph_aguafv'])
+    a.houses_with_dranage = ageb['vph_drenaj']  / (1 + ageb['vph_drenaj']  + ageb['vph_nodren'] ) 
+    a.hundimientos = ageb['hund']
+    a.group_kmean = ageb['kmeans5']
+    a.disease_burden = (ageb["n04_todas"] + ageb["x05_total"] + ageb["x06_todas"] + ageb["x07_todas"]+ ageb["x08_todas"]+ ageb["x09_todas"]+ ageb["n10_todas"]+ ageb["n11_todas"]+ ageb["n12_todas"] + ageb["n13_todas"] + ageb["n14_todas"]) / 11 
+    a.income_index = 0 if ageb['i05_ingres'] == 'nobody' else ageb['i05_ingres'] #nobody????
+    a.flooding = 0 if ageb['mean_encha'] == 'nobody' else ageb['mean_encha'] #?????????????????????????????'
+    a.antiguedad_infra = ageb['edad_infra']
+    a.zona_aquifera = ageb['zonas']
     
     
     session.add(a)
