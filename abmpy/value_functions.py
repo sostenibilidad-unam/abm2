26ee4bdada032e628c2b638345e44401dd3f6514
serranoycandela <serranoycandela@gmail.com>
2017-05-24 11:24:47 -0500

    inverted lineal function added patched

--- abmpy/value_functions.py
+++ abmpy/value_functions.py
@@ -1,10 +1,10 @@
-def lineal_invertida(x, min_x, max_x):
+def lineal_inverted(x, min_x, max_x):
     m = 1.0 / (max_x - min_x)
-    return max_x -  (m * x)
+    return 1.0 -  (m * x)
 
 def lineal(x, min_x, max_x):
     m = 1.0 / (max_x - min_x)
-    return m * x + min_x
+    return (m * x) + min_x
 
 
 def identity(x):
